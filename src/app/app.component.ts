6624bf8dbb35779d7b800dd607e4e60cf6fbc997
badia329 <badiychadly07@gmail.com>
2025-11-17 13:32:40 +0100

    first upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from "./components/footer/footer.component";
+import { BlogComponent } from './blog/blog.component';
+import { NewsComponent } from './news/news.component';
+import { StandingsComponent } from './standings/standings.component';
+import { CupEventsComponent } from './cup-events/cup-events.component';
+import { ResultComponent } from "./result/result.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, NewsComponent, FooterComponent, BlogComponent, StandingsComponent, CupEventsComponent, ResultComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/cup-events/cup-events.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cup-events',
+  imports: [],
+  templateUrl: './cup-events.component.html',
+  styleUrl: './cup-events.component.css'
+})
+export class CupEventsComponent {
+
+}
