b3628523fbfe0e6b9abcddf051c9a3d2b7790959
badia329 <badiychadly07@gmail.com>
2025-11-24 19:49:21 +0100

    fix: update add-match form

--- src/app/components/add-match/add-match.component.ts
+++ src/app/components/add-match/add-match.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-match',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-match.component.html',
   styleUrl: './add-match.component.css'
 })
 export class AddMatchComponent {
-
+  obj: any =  {};
+  addMatch() {
+    console.log("Here is  match Obj", this.obj);
+  }
 }
