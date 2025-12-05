5549ce3eb5b8500608adbc6e3fdc1a38bc3dcb7e
badi3 <badiychadly07@gmail.com>
2025-12-05 12:39:10 +0100

    save1

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -6,7 +6,8 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { confirmPasswordValidator } from '../../shared/confirm-password.validator';
 
 @Component({
   selector: 'app-signup',
@@ -16,16 +17,24 @@ import { CommonModule} from '@angular/common';
 })
 export class SignupComponent {
   signupForm!: FormGroup;
+
   constructor(private formBuilder: FormBuilder) {}
+
   ngOnInit() {
     console.log('Here sign Up');
-    this.signupForm = this.formBuilder.group({
-      firstName: ['', [Validators.required, Validators.minLength(3)]],
-      lastName: ['', [Validators.required, Validators.minLength(4)]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', Validators.required],
-      phone: ['', [Validators.required, Validators.pattern('^[0-9]{8}$')]],
-    });
+
+    this.signupForm = this.formBuilder.group(
+      {
+        firstName: ['', [Validators.required, Validators.minLength(3)]],
+        lastName: ['', [Validators.required, Validators.minLength(4)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+        phone: ['', [Validators.required, Validators.pattern('^[0-9]{8}$')]],
+      },
+      {
+        validators: confirmPasswordValidator,
+      }
+    );
   }
 }
