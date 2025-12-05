65fa4bd95af5b0746dbc86dd2b619c2193dd2e7b
badia329 <badiychadly07@gmail.com>
2025-12-05 11:26:01 +0100

    save1

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { BannerComponent } from "../banner/banner.component";
+import { BannerComponent } from '../banner/banner.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-signup',
-  imports: [BannerComponent],
+  imports: [BannerComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
 export class SignupComponent {
-
+  signupForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit() {
+    console.log('Here sign Up');
+    this.signupForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]{8}$')]],
+    });
+  }
 }
